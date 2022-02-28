d9cd5647df2b16a63e35d1d03b46de8fea8d5e6f
“Fareedh <2617283y@student.gla.ac.uk>
2022-02-28 19:13:22 +0000

    Templates skeleton

--- radar_project/radar/urls.py
+++ radar_project/radar/urls.py
@@ -5,4 +5,13 @@ app_name = 'radar'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('homepage1/', views.homepage1, name='homepage1'),
+    path('homepage2/', views.homepage2, name='homepage2'),
+    path('friendspage/', views.friendspage, name='friendspage'),
+    path('account/', views.account, name='account'),
+
 ]
