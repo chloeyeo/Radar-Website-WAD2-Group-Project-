ab99ec6ac66f125d58657c5d1801cfa4867a7f17
Chloe Yeo <2458600y@student.gla.ac.uk>
2022-03-05 18:14:44 +0000

    changed from user to userprofile fk

--- radar_project/radar/models.py
+++ radar_project/radar/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class FriendList(models.Model):
+    
+    userId_fk = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    friendId_fk = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.friendId_fk
+
+class Session(models.Model):
+    
+    private = models.BooleanField(default=True)
+    hasArrived = models.BooleanField(default = False)
+    destination = models.CharField(max_length=128, unique=True)
+    userId_fk = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.destination
