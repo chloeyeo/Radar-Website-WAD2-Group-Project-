7c832433c765aaf05fc42a1f5c3e8a91582d16fc
“Fareedh <2617283y@student.gla.ac.uk>
2022-03-07 20:37:52 +0000

    Rearranged code in models and updated the foreignkey usage

---
+++ radar_project/radar/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from radar.models import UserProfile
+from django import forms
+
+# Please update basing on the requirements of our implementation
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('picture', 'host',)
--- radar_project/radar/models.py
+++ radar_project/radar/models.py
@@ -1,35 +1,39 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # The additional attributes we wish to include.
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    host = models.BooleanField(default=False)
+    age = models.IntegerField(default=0)
+    shareLocation = models.BooleanField(default=False)
+    request = models.BooleanField(default=False)
+    accept = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+
 class FriendList(models.Model):
-    
-    userId_fk = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    friendId_fk = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    userId_fk = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.friendId_fk
 
+
 class Session(models.Model):
-    
     private = models.BooleanField(default=True)
-    hasArrived = models.BooleanField(default = False)
+    hasArrived = models.BooleanField(default=False)
     destination = models.CharField(max_length=128, unique=True)
-    userId_fk = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    userId_fk = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE,)
 
     def __str__(self):
         return self.destination
-
-class UserProfile(models.Model):
-# This line is required. Links UserProfile to a User model instance.
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-# The additional attributes we wish to include.
-    picture = models.ImageField(upload_to='profile_images', blank=True)
-    host = models.Boolean(default=False)
-    age = models.IntegerField(default=0)
-    shareLocation = models.BooleanField(default=False)
-    request = models.BooleanField(default=False)
-    accept = models.BooleanField(False)
-    
-    def __str__(self):
-        return self.user.username
