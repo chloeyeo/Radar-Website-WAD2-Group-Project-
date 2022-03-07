3bf45c79a75103195cbfa6913731501c543677d3
Chloe Yeo <2458600y@student.gla.ac.uk>
2022-03-07 22:37:02 +0000

    changes made to populate, but still not sure with friendlist

--- radar_project/populate_rango.py
+++ radar_project/populate_rango.py
@@ -17,3 +17,14 @@ def populate():
     friend_list = [ 
 
     sessions_in_area = {
+
+def add_userProfile(userId, firstName, lastName, age, shareLocation):
+    user = UserProfile.objects.get_or_create(userId = userId,
+                                             firstName = firstName,
+                                             lastName = lastName, age=age)
+    user.shareLocation = shareLocation
+    user.save()
+    return user
+
+def add_friend(userId, firstName, lastName, age, shareLocation=True):
+    friend = FriendList.objects.get_or_create(userId = userId
