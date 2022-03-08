f6a6c5b481e76ae963ff8614bf6848a6ab53ce44
Chloe Yeo <2458600y@student.gla.ac.uk>
2022-03-08 12:59:58 +0000

    made initial.py same as the one in main

--- radar_project/radar/migrations/0001_initial.py
+++ radar_project/radar/migrations/0001_initial.py
@@ -37,4 +37,11 @@ class Migration(migrations.Migration):
                 ('userId', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
+         migrations.CreateModel(
+            name='FriendList',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('friendId_fk', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='radar.UserProfile')),
+            ],
+        ),
     ]
