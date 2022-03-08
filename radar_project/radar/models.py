6d4cbc34d6ab54845a63ae0abe471ad96802ecd6
Chloe Yeo <2458600y@student.gla.ac.uk>
2022-03-08 13:13:59 +0000

    removed a space line at the top of model.py which was difference with model.py in main - hopefully this solves the other conflict if its line ending related?

--- radar_project/radar/models.py
+++ radar_project/radar/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
-    
     # This line is required. Links UserProfile to a User model instance.
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # The additional attributes we wish to include.
