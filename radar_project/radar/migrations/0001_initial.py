991c9647ae03118d4f706cda73ebc3a8ec553525
Chloe Yeo <2458600y@student.gla.ac.uk>
2022-03-08 13:35:23 +0000

    edited 0001 initial.py on my branch, migration now works

--- radar_project/radar/migrations/0001_initial.py
+++ radar_project/radar/migrations/0001_initial.py
@@ -18,12 +18,16 @@ class Migration(migrations.Migration):
             name='UserProfile',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=128, unique=True)), # added from main 
+                ('password', models.CharField(max_length=128, unique=True)), # added from main 
+                ('email', models.EmailField(max_length=254)), # added to main
                 ('picture', models.ImageField(blank=True, upload_to='profile_images')),
                 ('host', models.BooleanField(default=False)),
                 ('age', models.IntegerField(default=0)),
                 ('shareLocation', models.BooleanField(default=False)),
                 ('request', models.BooleanField(default=False)),
                 ('accept', models.BooleanField(verbose_name=False)),
+                # user is what my branch had and main didn't
                 ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
