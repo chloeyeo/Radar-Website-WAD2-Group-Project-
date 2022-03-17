006d3d83fcf38298e6e7f5be643a8b7011af1564
“Fareedh <2617283y@student.gla.ac.uk>
2022-03-17 21:26:12 +0000

    added account functionality

--- radar_project/radar/views.py
+++ radar_project/radar/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
-from radar.models import Post, Category
+from radar.models import Post, Category, UserProfile
+
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -20,17 +21,15 @@ def show_category(request, category_name_slug):
     return render(request, 'radar/category.html', context=context_dict)
 
 
-
 def homepage1(request):
     return render(request, 'radar/homepage1.html')
-
 
 
 def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-    
+
         user = authenticate(username=username, password=password)
         if user:
             if user.is_active:
@@ -47,40 +46,50 @@ def user_login(request):
 
 
 def signup(request):
-    
+
     registered = False
-    
+
     if request.method == 'POST':
         user_form = UserForm(request.POST)
         profile_form = UserProfileForm(request.POST)
-        
+
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save()
             user.set_password(user.password)
             user.save()
-            
+
             profile = profile_form.save(commit=False)
             profile.user = user
-            
+
             if 'picture' in request.FILES:
                 profile.picture = request.FILES['picture']
-                
+
             profile.save()
-            
+
             registered = True
         else:
             print(user_form.errors, profile_form.errors)
-            
+
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
-    return render(request, 'radar/signup.html', context = {'user_form': user_form,
-                                                            'profile_form':profile_form,
-                                                            'registered': registered})
+    return render(request, 'radar/signup.html', context={'user_form': user_form,
+                                                         'profile_form': profile_form,
+                                                         'registered': registered})
 
-@login_required
-def account(request):
-    return render(request, 'radar/account.html')
+
+# @login_required
+def account(request, current_user_slug):
+    context_dict = {}
+    current_user = request.user
+    try:
+        userProfile = UserProfile.objects.get(slug=current_user_slug)
+        context_dict['user'] = userProfile
+        print(userProfile.user.username)
+    except UserProfile.DoesNotExist:
+        context_dict['user'] = None
+    return render(request, 'radar/account.html', context=context_dict)
+
 
 @login_required
 def user_logout(request):
