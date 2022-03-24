282999dd13a95be9948d5538b96166066b949786
“Fareedh <2617283y@student.gla.ac.uk>
2022-03-24 02:41:54 +0000

    like is now fully using ajax

--- radar_project/static/js/radar-ajax.js
+++ radar_project/static/js/radar-ajax.js
@@ -1,14 +1,21 @@
 $(document).ready(function() { 
 
-    $("div.card").find('button').click(function(){
+    $(".like_post_btn").click(function(){
         var getPostId;
-        
         getPostId = $(this).attr('post-id');
         $.get('/radar/like_post/', {'post_id': getPostId},
         function(data){
             var name = data.post_id
             var selector = document.getElementsByName(name);
+            
             $(selector).html("Likes : " + data.total_likes);
+
+            // why isn't the button updating
+            if(data.liked){
+                $(this).html("Liked");
+            }else{
+                $(this).html("Like");
+            }
         }
         );
     });
