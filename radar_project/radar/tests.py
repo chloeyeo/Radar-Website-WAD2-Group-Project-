543b2901ca280ab00add63b0f7268aef9aa99609
Chloe Yeo <2458600y@student.gla.ac.uk>
2022-03-26 17:48:33 +0000

    added slug test under post test in tests.py

--- radar_project/radar/tests.py
+++ radar_project/radar/tests.py
@@ -5,26 +5,35 @@ from django.contrib.auth.models import User
 # Create your tests here.
 
 class CategoryMethodTests(TestCase):
-    def test_ensure_views_are_positive(self):
+    def test_ensure_category_views_are_positive(self):
         '''Ensures the number of views received for a Category are positive or zero.'''
         category = Category(name='test', views=-1)
         category.save()
         self.assertEqual((category.views >= 0), True)
 
-    def test_slug_line_creation(self):
+    def test_category_slug_line_creation(self):
         ''' Checks to make sure that when a category is created, an appropriate slug is created. Example: "Random Category String" should be "random-category-string". '''
         category = Category(name='Random Category String')
         category.save()
         self.assertEqual(category.slug, 'random-category-string')
 
 class PostMethodTests(TestCase):
-    def test_ensure_views_are_positive(self):
+    def test_ensure_post_views_are_positive(self):
         '''Ensures the number of views received for a Post is positive or zero.'''
         User.posts=0
         post = Post(title='test', views=-1)
-        category = Category(name='test', views=-1)
+        category = Category(name='test')
         category.save()
         post.category_id = category.id
         post.save()
         self.assertEqual((post.views >= 0), True)
+
+    def test_post_slug_line_creation(self):
+        ''' Checks to make sure that when a post is created, an appropriate slug is created. Example: "Random Category String" should be "random-category-string". '''
+        post = Post(title='Random Category String')
+        category = Category(name='test')
+        category.save()
+        post.category_id = category.id
+        post.save()
+        self.assertEqual(post.slug, 'random-category-string')
         
