5f4c33d605fb96883162b3809352e0ddd4840f3f
“Fareedh <2617283y@student.gla.ac.uk>
2022-04-15 13:35:45 +0100

    fixed like when not logged in

--- radar_project/static/js/radar-jquery.js
+++ radar_project/static/js/radar-jquery.js
@@ -1,8 +1,6 @@
 $(document).ready(function() { 
     $('.login_to_like').click(function(){
-        $(this).click(function(){
             alert("Login to access this feature");
-        })
         
     });
 
